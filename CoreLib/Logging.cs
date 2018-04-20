c13127845a1a93abb0d17f054320f3ea63efea89
Alex Voskresenskiy <advoskr@gmail.com>
2018-04-20 20:41:39 +0300

    Пробник собирается и запускает в докер-компоузе

---
+++ CoreLib/Logging.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace CoreLib
+{
+    public class Logging
+    {
+        public static void LogInfo(string str)
+        {
+            LogManager.GetCurrentClassLogger().Info(str);
+        }
+
+        public static void Configure()
+        {
+            var config = new LoggingConfiguration();
+
+            var target =
+                new FileTarget
+                {
+                    FileName = "${basedir}/"+typeof(Logging).FullName + ".log"
+                };
+
+            config.AddTarget("logfile", target);
+            
+            var rule = new LoggingRule("*",LogLevel.Info, target);
+            config.LoggingRules.Add(rule);
+
+            LogManager.Configuration = config;
+
+            LogManager.GetCurrentClassLogger().Debug("Using programmatic config");
+        }
+    }
+}
---
+++ DockerizedSimpleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using CoreLib;
+
+namespace DockerizedSimpleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logging.Configure();
+            Logging.LogInfo("This shit works");
+            Console.WriteLine("Hello World!");
+            Logging.LogInfo("This shit printed");
+            var filePath = Directory.GetCurrentDirectory()+"/test.log";
+            var fileDesc = File.Create(filePath);
+            using (var writer = new StreamWriter(fileDesc))
+            {
+                writer.WriteLine("Blabla");
+            }
+            fileDesc.Close();
+            Console.WriteLine(filePath);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            DirectoryInfo d = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var files = d.GetFiles("*"); //Getting Text files
+            string str = "";
+            foreach (FileInfo file in files)
+            {
+                str = str + ", " + file.Name;
+            }
+            Console.WriteLine("Files:"+str);
+            Logging.LogInfo("This shit turned off");
+            Console.ReadLine();
+        }
+    }
+}
