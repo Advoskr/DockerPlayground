8878a3fc0a8f95401b0641574f406e37bd734ad8
Alex Voskresenskiy <advoskr@gmail.com>
2018-04-26 20:09:56 +0300

    Working sample with dockerized nginx, pg, pgadmin, rmq and simple netcore app, building in docker container

---
+++ CoreLib/DbTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Npgsql;
+
+namespace CoreLib
+{
+    public static class DbTest
+    {
+        public static (bool, string) TestConnection(string dbConnStr)
+        {
+            try
+            {
+                using (var conn = new NpgsqlConnection(dbConnStr))
+                {
+                    conn.Open();
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "Select 1 as bit";
+                        var res = (int)cmd.ExecuteScalar();
+                        return res!=1 ? (false,"db check result is not same as expected") : (true, "");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return (false, e.Message);
+            }
+            
+        }
+        
+    }
+}
--- DockerizedSimpleApp/Program.cs
+++ DockerizedSimpleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using CoreLib;
+using EasyNetQ;
 
 namespace DockerizedSimpleApp
 {
@@ -30,6 +32,54 @@ namespace DockerizedSimpleApp
             }
             Console.WriteLine("Files:"+str);
             Logging.LogInfo("This shit turned off");
+            
+            var maxRetries = 10;
+            for (int tryCount = 0; tryCount < maxRetries; tryCount++)
+            {
+                Console.WriteLine($"Attempt to connect to db #{tryCount}");
+                var (isOk, message) = DbTest.TestConnection("User ID=postgres;Password=postgres;Host=postgres;Port=5432;");
+                if (!isOk)
+                    Console.WriteLine(message);
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Successfully connected to DB!");
+                    Console.ResetColor();
+                    break;
+                }
+            }
+            
+            //while (true)
+            //{
+            //    Console.WriteLine($"Checking DB State");
+            //    var (isOk, message) = DbTest.TestConnection("User ID=postgres;Password=postgres;Host=postgres;Port=5432;");
+            //    if (!isOk)
+            //        Console.WriteLine(message);
+            //    else
+            //    {
+            //        Console.WriteLine("Successfully connected to DB!");
+            //    }
+            //    Thread.Sleep(2000);
+            //}
+            
+            for (int tryCount = 0; tryCount < maxRetries; tryCount++)
+            {
+                Console.WriteLine($"Try #{tryCount}");
+                using (var bus = RabbitHutch.CreateBus("host=rabbitmq;username=rmq;password=azsxdc!2;virtualHost=vhost"))
+                {
+                    if (!bus.IsConnected)
+                        Console.WriteLine("Bus is not connected, check RMQ");
+                    else
+                    {
+                        Console.WriteLine($"Rmq bus status: {(bus.IsConnected ? "ok" : "fail")}");
+                        bus.Advanced.QueueDeclare("TestQueue", false, durable: true, exclusive: false,
+                            autoDelete: true);
+                        break;
+                    }
+                }
+                Thread.Sleep(2000);
+            }
+
             Console.ReadLine();
         }
     }
